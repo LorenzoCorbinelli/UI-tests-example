550f73f53d70268d7128e356b9555ba45ba7e59a
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-04-18 16:48:59 +0200

    Set the title of the frame

--- school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
+++ school/src/main/java/com/examples/school/view/swing/StudentSwingView.java
@@ -31,6 +31,7 @@ public class StudentSwingView extends JFrame {
 	 * Create the frame.
 	 */
 	public StudentSwingView() {
+		setTitle("Student View");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
