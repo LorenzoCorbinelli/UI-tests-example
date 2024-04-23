d95ee49b8915b4ae4504bda39e9c9858e2fa0abe
Lorenzo Corbinelli <lore.corbi01@gmail.com>
2024-04-23 16:37:11 +0200

    Parsing arguments with Picocli

--- school/src/main/java/com/examples/school/app/swing/SchoolSwingApp.java
+++ school/src/main/java/com/examples/school/app/swing/SchoolSwingApp.java
@@ -1,6 +1,7 @@
 package com.examples.school.app.swing;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
 
 import com.examples.school.controller.SchoolController;
 import com.examples.school.repository.mongo.StudentMongoRepository;
@@ -8,17 +9,30 @@ import com.examples.school.view.swing.StudentSwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class SchoolSwingApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(mixinStandardHelpOptions = true)
+public class SchoolSwingApp implements Callable<Void>{
+	
+	@Option(names = {"--mongo-host"}, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+	
+	@Option(names = {"--db-name"}, description = "Database name")
+	private int mongoPort = 27017;
+	
+	@Option(names = {"--db-collection"}, description = "Collection name")
+	private String collectionName = "student";
 
 	public static void main(String[] args) {
+		new CommandLine(new SchoolSwingApp()).execute(args);
+	}
+
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
-				String mongoHost = "localhost";
-				int mongoPort = 27017;
-				if(args.length > 0)
-					mongoHost = args[0];
-				if(args.length > 1)
-					mongoPort = Integer.parseInt(args[1]);
 				StudentMongoRepository studentRepository = new StudentMongoRepository(
 						new MongoClient(new ServerAddress(mongoHost, mongoPort)), "school", "student");
 				StudentSwingView studentView = new StudentSwingView();
@@ -30,6 +44,7 @@ public class SchoolSwingApp {
 				e.printStackTrace();
 			}
 		});
+		return null;
 	}
 
 }
